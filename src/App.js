b514c29c93eda99d36c6eb2123aff6e548fdc4d1
Akash1671998 <akash.vishwakarma@coraltele.com>
2025-06-03 14:42:03 +0530

    fix all component moved on inside src folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const connectDB = require("../Connections/connetion");
-const AuthRouter = require("../Routes/AuthRouters");
-const ProductRouter = require("../Routes/ProductRouter");
-const ExpenseRouter = require("../Routes/ExpenseRouter");
+const connectDB = require("./Connections/connetion");
+const AuthRouter = require("./Routes/AuthRouters");
+const ProductRouter = require("./Routes/ProductRouter");
+const ExpenseRouter = require("./Routes/ExpenseRouter");
 
 const cors = require("cors");
-const { ensureAuthenticated } = require("../Middlewares/Auth");
+const { ensureAuthenticated } = require("./Middlewares/Auth");
 const PORT = 9090;
 
 const app = express();
